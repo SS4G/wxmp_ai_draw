a7c566ee1e8292feb473af620729cfab6fa3c5e8
songzihengs.666 <songziheng.666@bytedance.com>
2023-07-09 09:59:30 +0800

    [archive] 聊天小程序终结

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,6 +8,18 @@ Page({
     rsp_text: "null"
   },
 
+  onShareTimeline: function(options) {
+    var that = this;
+    // 设置菜单中的转发按钮触发转发事件时的转发内容
+    var shareObj = {
+      title: "最聪明的人工智能聊天机器人",    // 默认是小程序的名称(可以写slogan等)
+      path: '/pages/index/index',    // 默认是当前页面，必须是以‘/'开头的完整路径
+      imageUrl: '../../imgs/gptavtar.png',   //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
+    };
+    // 返回shareObj
+    return shareObj;
+  },
+
   onShareAppMessage: function( options ){
     var that = this;
     // 设置菜单中的转发按钮触发转发事件时的转发内容
